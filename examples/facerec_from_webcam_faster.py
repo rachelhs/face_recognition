44cbf22ccb159c7421e50fc40ce0296b7c187229
rachelhs <rachelsmith400@hotmail.co.uk>
2019-05-30 17:55:58 +0100

    Update facerec_from_webcam_faster.py

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -14,23 +14,26 @@ import numpy as np
 # Get a reference to webcam #0 (the default one)
 video_capture = cv2.VideoCapture(0)
 
-# Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
-obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
+#make array of sample pictures with encodings
+#add people to 'known_people' folder to extend number of recognisable faces
+known_face_encodings = []
+known_face_names = []
+dirname = os.path.dirname(__file__)
+path = os.path.join(dirname, 'known_people/')
 
-# Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+#find number of known faces
+list_of_files = os.listdir(path)
+number_files = len(list_of_files)
 
-# Create arrays of known face encodings and their names
-known_face_encodings = [
-    obama_face_encoding,
-    biden_face_encoding
-]
-known_face_names = [
-    "Barack Obama",
-    "Joe Biden"
-]
+for i in range(number_files-1):
+	globals()['image_{}'.format(i)] = face_recognition.load_image_file(path + os.listdir(path)[i+1])
+	globals()['image_encoding_{}'.format(i)] = face_recognition.face_encodings(globals()['image_{}'.format(i)])[0]
+
+	known_face_encodings.append(globals()['image_encoding_{}'.format(i)])
+
+	# Create array of known names
+
+	known_face_names.append(list_of_files[i+1])
 
 # Initialize some variables
 face_locations = []
